c88488fcddc0af770d691fc4fd850d573563ff8c
nick <bnxaxnb@gmail.com>
2021-03-02 19:13:27 +0200

    Updating API and db operations

--- includes/DbOperation.php
+++ includes/DbOperation.php
@@ -1,1 +1,149 @@
 <?php
+
+class DbOperation
+{
+    //Database connection link
+    private $con;
+
+    //Class constructor
+    function __construct()
+    {
+        //Getting the DbConnect.php file
+        require_once dirname(__FILE__) . '/DbConnect.php';
+
+        //Creating a DbConnect object to connect to the database
+        $db = new DbConnect();
+
+        //Initializing our connection link of this class
+        //by calling the method connect of DbConnect class
+        $this->con = $db->connect();
+    }
+
+
+    /*
+    * The read operation
+    * When this method is called it is returning all the existing record of the database
+    */
+    function getLessons(){
+        $stmt = $this->con->prepare("SELECT idtimetable, Subject, Date, Day, Week, Teacher, Group, Course, Time FROM timetable");
+        $stmt->execute();
+        $stmt->bind_result($idtimetable, $Subject, $Date, $Day, $Week, $Teacher, $Group, $Course, $Time);
+
+        $lessons = array();
+
+        while($stmt->fetch()){
+            $lesson  = array();
+            $lesson['idtimetable'] = $idtimetable;
+            $lesson['Subject'] = $Subject;
+            $lesson['Date'] = $Date;
+            $lesson['Day'] = $Day;
+            $lesson['Week'] = $Week;
+            $lesson['Teacher'] = $Teacher;
+            $lesson['Group'] = $Group;
+            $lesson['Course'] = $Course;
+            $lesson['Time'] = $Time;
+
+            array_push($lessons, $lesson);
+        }
+
+        return $lessons;
+    }
+
+    function getSubjects(){
+        $stmt = $this->con->prepare("SELECT idsubject, name FROM subject");
+        $stmt->execute();
+        $stmt->bind_result($idsubject, $name);
+
+        $subjects = array();
+
+        while($stmt->fetch()){
+            $subject  = array();
+            $subject['idsubject'] = $idsubject;
+            $subject['name'] = $name;
+
+
+            array_push($subjects, $subject);
+        }
+
+        return $subjects;
+    }
+
+    function getGroups(){
+        $stmt = $this->con->prepare("SELECT idgroup, name FROM group");
+        $stmt->execute();
+        $stmt->bind_result($idgroup, $name);
+
+        $groups = array();
+
+        while($stmt->fetch()){
+            $group  = array();
+            $group['idgroup'] = $idgroup;
+            $group['name'] = $name;
+
+
+            array_push($groups, $group);
+        }
+
+        return $groups;
+    }
+
+    function getCourse(){
+        $stmt = $this->con->prepare("SELECT idcourse, number FROM course");
+        $stmt->execute();
+        $stmt->bind_result($idcourse, $number);
+
+        $courses = array();
+
+        while($stmt->fetch()){
+            $course  = array();
+            $course['idcourse'] = $idcourse;
+            $course['number'] = $number;
+
+
+            array_push($courses, $course);
+        }
+
+        return $courses;
+    }
+
+    function getDays(){
+        $stmt = $this->con->prepare("SELECT idDay, name FROM day");
+        $stmt->execute();
+        $stmt->bind_result($idDay, $name);
+
+        $days = array();
+
+        while($stmt->fetch()){
+            $day  = array();
+            $day['idDay'] = $idDay;
+            $day['name'] = $name;
+
+
+            array_push($lessons, $day);
+        }
+
+        return $days;
+    }
+
+    function getTeachers(){
+        $stmt = $this->con->prepare("SELECT idTeacher, name, mail FROM teacher");
+        $stmt->execute();
+        $stmt->bind_result($idTeacher, $name, $mail);
+
+        $teachers = array();
+
+        while($stmt->fetch()){
+            $teacher  = array();
+            $teacher['idTeacher'] = $idTeacher;
+            $teacher['name'] = $name;
+            $teacher['mail'] = $mail;
+
+
+            array_push($teachers, $teacher);
+        }
+
+        return $teachers;
+    }
+
+
+}
--- v1/Api.php
+++ v1/Api.php
@@ -1,1 +1,96 @@
 <?php
+
+//getting the dboperation class
+require_once '../includes/DbOperation.php';
+
+//function validating all the paramters are available
+//we will pass the required parameters to this function
+function isTheseParametersAvailable($params){
+    //assuming all parameters are available
+    $available = true;
+    $missingparams = "";
+
+    foreach($params as $param){
+        if(!isset($_POST[$param]) || strlen($_POST[$param])<=0){
+            $available = false;
+            $missingparams = $missingparams . ", " . $param;
+        }
+    }
+
+    //if parameters are missing
+    if(!$available){
+        $response = array();
+        $response['error'] = true;
+        $response['message'] = 'Parameters ' . substr($missingparams, 1, strlen($missingparams)) . ' missing';
+
+        //displaying error
+        echo json_encode($response);
+
+        //stopping further execution
+        die();
+    }
+}
+
+//an array to display response
+$response = array();
+
+//if it is an api call
+//that means a get parameter named api call is set in the URL
+//and with this parameter we are concluding that it is an api call
+if(isset($_GET['apicall'])){
+
+    switch($_GET['apicall']){
+        //the READ operation
+        //if the call is getheroes
+        case 'getLessons':
+            $db = new DbOperation();
+            $response['error'] = false;
+            $response['message'] = 'Request successfully completed';
+            $response['lessons'] = $db->getLessons();
+            break;
+
+        case 'getGroups':
+            $db = new DbOperation();
+            $response['error'] = false;
+            $response['message'] = 'Request successfully completed';
+            $response['groups'] = $db->getGroups();
+            break;
+
+        case 'getTeacher':
+            $db = new DbOperation();
+            $response['error'] = false;
+            $response['message'] = 'Request successfully completed';
+            $response['teachers'] = $db->getTeachers();
+            break;
+
+        case 'getSubjects':
+            $db = new DbOperation();
+            $response['error'] = false;
+            $response['message'] = 'Request successfully completed';
+            $response['subjects'] = $db->getSubjects();
+            break;
+
+        case 'getDays':
+            $db = new DbOperation();
+            $response['error'] = false;
+            $response['message'] = 'Request successfully completed';
+            $response['days'] = $db->getDays();
+            break;
+
+        case 'getCourses':
+            $db = new DbOperation();
+            $response['error'] = false;
+            $response['message'] = 'Request successfully completed';
+            $response['courses'] = $db->getCourse();
+            break;
+    }
+
+}else{
+    //if it is not api call
+    //pushing appropriate values to response array
+    $response['error'] = true;
+    $response['message'] = 'Invalid API Call';
+}
+
+//displaying the response in json structure
+echo json_encode($response);
